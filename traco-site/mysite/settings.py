e910d63f16cc956569d337a558c35faceb72e6f9
MonishaJ1997 <jkmonisha97@gmail.com>
2025-09-08 01:16:01 +0530

    Update settings.py

--- traco-site/mysite/settings.py
+++ traco-site/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uibdbp#@z(4d0l#6o6!&g@t0vaay0=j4ngvdb0mzi=3oabe@pc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
